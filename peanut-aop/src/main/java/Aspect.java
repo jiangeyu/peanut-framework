8f688d7addd7a4e29dad69a5be6323704d97d7c3
pjx <lingxiao@2dfire.com>
2018-07-13 23:46:17 +0800

    aop  注解定义

---
+++ peanut-aop/src/main/java/Aspect.java
@@ -0,0 +1,14 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午11:43 2018/7/13
+ * @desc
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Aspect {
+}
