47e21f3994a98b4fe58d7cabf1ee12d9344c0bba
pjx <lingxiao@2dfire.com>
2018-07-16 23:56:03 +0800

    切面代理

--- peanut-aop/src/main/java/AopHelper.java
+++ peanut-aop/src/main/java/AopHelper.java
@@ -1,7 +1,7 @@
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:49 2018/7/13
- * @desc
+ * @desc 初始化aop框架
  */
 public class AopHelper {
 }
--- peanut-aop/src/main/java/AspectProxy.java
+++ peanut-aop/src/main/java/AspectProxy.java
@@ -1,7 +1,7 @@
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:49 2018/7/13
- * @desc
+ * @desc 切面代理
  */
 public class AspectProxy {
 }
