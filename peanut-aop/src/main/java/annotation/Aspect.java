969a24d3b67840affdca37418bbb163eb441e5d5
pjx <lingxiao@2dfire.com>
2018-07-16 23:18:33 +0800

    切面接口定义

--- peanut-aop/src/main/java/annotation/Aspect.java
+++ peanut-aop/src/main/java/annotation/Aspect.java
@@ -1,16 +1,20 @@
 package annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:43 2018/7/13
- * @desc
+ * @desc 定义切面类
  */
+
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Aspect {
+
+    String pkg() default "";
+
+    String cls() default "";
+
+    Class<? extends Annotation> annotion() default Aspect.class;
 }
--- peanut-aop/src/main/java/annotation/AspectOrder.java
+++ peanut-aop/src/main/java/annotation/AspectOrder.java
@@ -8,9 +8,11 @@ import java.lang.annotation.Target;
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:44 2018/7/13
- * @desc
+ * @desc 定义切面顺序
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AspectOrder {
+
+    int value();
 }
