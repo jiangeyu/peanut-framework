7e070981b2450e13577cc78f8a29db1cee688900
pjx <lingxiao@2dfire.com>
2018-07-18 00:25:45 +0800

    代理管理器

--- peanut-aop/src/main/java/proxy/ProxyChain.java
+++ peanut-aop/src/main/java/proxy/ProxyChain.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,15 +23,16 @@ public class ProxyChain {
     @Getter
     private final Object[] methodParams;
 
-    private List<Proxy> proxyList = new ArrayList<Proxy>();
+    private List<Proxy> proxyList;
     private int proxyIndex = 0;
 
-    public ProxyChain(Class<?> targetClass, Object targetObject, Method targetMethod, MethodProxy methodProxy, Object[] methodParams) {
+    public ProxyChain(Class<?> targetClass, Object targetObject, Method targetMethod, MethodProxy methodProxy, Object[] methodParams, List<Proxy> proxyList) {
         this.targetClass = targetClass;
         this.targetObject = targetObject;
         this.targetMethod = targetMethod;
         this.methodProxy = methodProxy;
         this.methodParams = methodParams;
+        this.proxyList = proxyList;
     }
 
     public Object doProxyChain() throws Throwable {
--- peanut-aop/src/main/java/proxy/ProxyManager.java
+++ peanut-aop/src/main/java/proxy/ProxyManager.java
@@ -1,9 +1,25 @@
 package proxy;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:49 2018/7/13
- * @desc
+ * @desc 代理管理器
  */
 public class ProxyManager {
+
+    public static <T> T createProxy(final Class<?> targetClass, final List<Proxy> proxyList) {
+        return (T) Enhancer.create(targetClass, new MethodInterceptor() {
+
+            public Object intercept(Object targetObject, Method targetMethod, Object[] methodParams, MethodProxy methodProxy) throws Throwable {
+                return new ProxyChain(targetClass, targetObject, targetMethod, methodProxy, methodParams, proxyList).doProxyChain();
+            }
+        });
+    }
 }
