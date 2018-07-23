8aec9ea48aa443470c1cd1015bcd2fbe4edb4aec
pjx <lingxiao@2dfire.com>
2018-07-23 23:09:44 +0800

    base bean

--- peanut-core/src/main/java/bean/BaseBean.java
+++ peanut-core/src/main/java/bean/BaseBean.java
@@ -1,9 +1,29 @@
 package bean;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:55 2018/7/13
- * @desc
+ * @desc 提供 Bean 类的基础属性
  */
-public class BaseBean {
+public abstract class BaseBean {
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
 }
