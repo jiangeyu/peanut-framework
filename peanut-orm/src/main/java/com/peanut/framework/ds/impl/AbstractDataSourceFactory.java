7c1fc6419c42da850b6567391474c983439a8761
pjx <lingxiao@2dfire.com>
2018-07-25 23:19:47 +0800

    数据源工厂

---
+++ peanut-orm/src/main/java/com/peanut/framework/ds/impl/AbstractDataSourceFactory.java
@@ -0,0 +1,9 @@
+package com.peanut.framework.ds.impl;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午11:18 2018/7/25
+ * @desc
+ */
+public class AbstractDataSourceFactory {
+}
