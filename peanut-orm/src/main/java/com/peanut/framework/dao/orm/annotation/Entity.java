abb97a6f142c156f47851700746234fd83b1f1a0
pjx <lingxiao@2dfire.com>
2018-07-25 23:30:01 +0800

    数据库字段

---
+++ peanut-orm/src/main/java/com/peanut/framework/dao/orm/annotation/Entity.java
@@ -0,0 +1,9 @@
+package com.peanut.framework.dao.orm.annotation;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午11:29 2018/7/25
+ * @desc
+ */
+public interface Entity {
+}
