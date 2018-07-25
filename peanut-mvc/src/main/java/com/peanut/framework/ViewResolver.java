dd3c868034974e357bda758a28f2e4ed47ee84a6
pjx <lingxiao@2dfire.com>
2018-07-25 23:24:32 +0800

    视图解析层

---
+++ peanut-mvc/src/main/java/com/peanut/framework/ViewResolver.java
@@ -0,0 +1,9 @@
+package com.peanut.framework;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午11:22 2018/7/25
+ * @desc
+ */
+public interface ViewResolver {
+}
