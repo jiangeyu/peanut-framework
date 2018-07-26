3e4ab371818d0d06807742ea2b0fbd733e10df9a
pjx <lingxiao@2dfire.com>
2018-07-26 23:48:28 +0800

    core

---
+++ peanut-mvc/src/main/java/com/peanut/framework/fault/UploadException.java
@@ -0,0 +1,9 @@
+package com.peanut.framework.fault;
+
+/**
+ * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
+ * @time: Created in 下午11:41 2018/7/25
+ * @desc
+ */
+public class UploadException {
+}
