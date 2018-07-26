8f012272ebf839f0f1fb6d0ce79848e1641690a4
pjx <lingxiao@2dfire.com>
2018-07-26 23:48:02 +0800

    core

--- peanut-core/src/main/java/com/peanut/framework/impl/support/AnnotationClassTmplate.java
+++ peanut-core/src/main/java/com/peanut/framework/impl/support/AnnotationClassTmplate.java
@@ -3,7 +3,7 @@ package com.peanut.framework.impl.support;
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:12 2018/7/24
- * @desc
+ * @desc 用于获取注解类的模板类
  */
 public class AnnotationClassTmplate {
 }
--- peanut-core/src/main/java/com/peanut/framework/impl/support/ClassTemplate.java
+++ peanut-core/src/main/java/com/peanut/framework/impl/support/ClassTemplate.java
@@ -3,7 +3,7 @@ package com.peanut.framework.impl.support;
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:13 2018/7/24
- * @desc
+ * @desc 用于获取类的模板类
  */
 public abstract class ClassTemplate {
 }
