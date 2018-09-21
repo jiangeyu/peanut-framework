72369a98bf09b13c42716e380458be78577c3f07
pjx <lingxiao@2dfire.com>
2018-09-21 23:26:59 +0800

    class template

--- peanut-core/src/main/java/com/peanut/framework/impl/support/ClassTemplate.java
+++ peanut-core/src/main/java/com/peanut/framework/impl/support/ClassTemplate.java
@@ -1,9 +1,24 @@
 package com.peanut.framework.impl.support;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:13 2018/7/24
  * @desc 用于获取类的模板类
  */
 public abstract class ClassTemplate {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClassTemplate.class);
+
+    protected final String packageName;
+
+    public ClassTemplate(String packageName) {
+        this.packageName = packageName;
+    }
+
+
+    public abstract boolean checkAddClass(Class<?> cls);
 }
