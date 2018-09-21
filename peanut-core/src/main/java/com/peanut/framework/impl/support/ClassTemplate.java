64c2cadcab6e9ccb6f7ca8cf93f0b00dda09fa4b
pjx <lingxiao@2dfire.com>
2018-09-22 01:04:08 +0800

    Class template

--- peanut-core/src/main/java/com/peanut/framework/impl/support/ClassTemplate.java
+++ peanut-core/src/main/java/com/peanut/framework/impl/support/ClassTemplate.java
@@ -1,12 +1,19 @@
 package com.peanut.framework.impl.support;
 
+import com.peanut.framework.util.ClassUtil;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.net.JarURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 
 /**
@@ -27,8 +34,30 @@ public abstract class ClassTemplate {
     public final List<Class<?>> getClassList() {
         List<Class<?>> classList = new ArrayList<Class<?>>();
         try {
-            Enumeration<URL> urls;
+            Enumeration<URL> urls = ClassUtil.getClassLoader().getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (url != null) {
+                    String protocol = url.getProtocol();
+                    if ("file".equals(protocol)) {
+                        String packagePath = url.getPath().replaceAll("%20", " ");
+                        addClass(classList, packagePath, packageName);
+                    } else if ("jar".equals(protocol)) {
+                        JarURLConnection jarURLConnection = (JarURLConnection) url.openConnection();
+                        JarFile jarFile = jarURLConnection.getJarFile();
+                        Enumeration<JarEntry> jarEntries = jarFile.entries();
+                        while (jarEntries.hasMoreElements()) {
+                            JarEntry jarEntry = jarEntries.nextElement();
+                            String jarEntryName = jarEntry.getName();
+                            if (".class".equals(jarEntryName)) {
+                                String className = jarEntryName.substring(0, jarEntryName.lastIndexOf(".")).replace("/", ".");
+                                doAddClass(classList, className);
+                            }
+                        }
 
+                    }
+                }
+            }
         } catch (Exception e) {
             logger.error("获取类出错! ", e);
         }
@@ -36,9 +65,46 @@ public abstract class ClassTemplate {
     }
 
     private void addClass(List<Class<?>> classList, String packagePath, String packageName) {
+        try {
+            File[] files = new File(packagePath).listFiles(new FileFilter() {
+                public boolean accept(File file) {
+                    return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
+                }
+            });
+            for (File file : files) {
+                String fileName = file.getName();
+                if (file.isFile()) {
+                    String className = fileName.substring(0, fileName.lastIndexOf("."));
+                    if (StringUtils.isNotEmpty(packageName)) {
+                        className = packageName + "." + className;
+                    }
+                    doAddClass(classList, className);
+                } else {
+
+                    String subPackagePath = fileName;
+                    if (StringUtils.isNotEmpty(packagePath)) {
+                        subPackagePath = packagePath + "." + subPackagePath;
+                    }
+
+                    String subPackageName = fileName;
+                    if (StringUtils.isNotEmpty(packageName)) {
+                        subPackageName = packageName + "." + subPackageName;
+                    }
+                    addClass(classList, subPackagePath, subPackageName);
+                }
+            }
+        } catch (Exception e) {
+            logger.error("添加类出错！", e);
+        }
 
     }
 
+    private void doAddClass(List<Class<?>> classList, String className) {
+        Class<?> cls = ClassUtil.loadClass(className, false);
+        if (checkAddClass(cls)) {
+            classList.add(cls);
+        }
+    }
 
     public abstract boolean checkAddClass(Class<?> cls);
 }
