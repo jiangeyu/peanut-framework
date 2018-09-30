a786b2b2b92c4870c1c452d90c0824fd0b91bf46
pjx <lingxiao@2dfire.com>
2018-09-30 09:58:46 +0800

    请求类型定义

--- peanut-mvc/src/main/java/com/peanut/framework/annotation/Request.java
+++ peanut-mvc/src/main/java/com/peanut/framework/annotation/Request.java
@@ -1,9 +1,56 @@
 package com.peanut.framework.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:23 2018/7/25
- * @desc
+ * @desc 定义请求
  */
-public interface Request {
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Request {
+
+    /**
+     * 定义GET请求
+     *
+     */
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Get {
+        String value();
+    }
+
+    /**
+     * 定义POST请求
+     *
+     */
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Post {
+        String value();
+    }
+
+    /**
+     * 定义PUT请求
+     *
+     */
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Put {
+        String value();
+    }
+
+    /**
+     * 定义DELETE请求
+     *
+     */
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @interface Delete {
+        String value();
+    }
 }
--- peanut-mvc/src/main/java/com/peanut/framework/impl/DafaultHandlerMapping.java
+++ peanut-mvc/src/main/java/com/peanut/framework/impl/DafaultHandlerMapping.java
@@ -3,7 +3,7 @@ package com.peanut.framework.impl;
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
  * @time: Created in 下午11:43 2018/7/25
- * @desc
+ * @desc 默认 Handler 调用器
  */
 public class DafaultHandlerMapping {
 }
