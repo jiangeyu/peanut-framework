9980144e66113b16195d55412bc873cc9a3a878a
pjx <lingxiao@2dfire.com>
2018-10-01 14:03:17 +0800

    webUtil

--- peanut-core/src/main/java/com/peanut/framework/util/WebUtil.java
+++ peanut-core/src/main/java/com/peanut/framework/util/WebUtil.java
@@ -1,6 +1,12 @@
 package com.peanut.framework.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author: <a href="mailto:lingxiao@2dfire.com">凌霄</a>
@@ -8,6 +14,30 @@ import javax.servlet.http.HttpServletRequest;
  * @desc
  */
 public class WebUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebUtil.class);
+
+    public static void writeJSON(HttpServletResponse response, Object data) {
+
+    }
+
+    public static void writeHTML(HttpServletResponse response,Object data) {
+
+    }
+
+    public static Map<String, Object> getRequestParamMap(HttpServletRequest request) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        return map;
+    }
+
+
+    public static boolean checkParamName(String paramName) {
+        return !paramName.equals("_");
+    }
+
+    public static void forwardRequest(String path, HttpServletRequest request, HttpServletResponse response) {
+
+    }
 
     public static String getRequestPath(HttpServletRequest request) {
         String path = request.getServletPath();
